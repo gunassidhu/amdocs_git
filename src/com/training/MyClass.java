d01588fdc60066cee53f11a4520eb73f3fedded5
amdoconb003 <amdoconb003@Amdonb197.iiht.tech>
2022-07-20 15:50:34 +0530

    updated MyClass

--- src/com/training/MyClass.java
+++ src/com/training/MyClass.java
@@ -4,7 +4,6 @@ public class MyClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World!! AMDOCS");
+		System.out.println("Hello World AMDOCS!!");
 	}
-
 }
